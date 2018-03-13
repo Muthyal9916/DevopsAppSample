7c1f6688c108af0cf6e758e984f19d3192c0444f
muthu <muthuatp.k@gmail.com>
2018-03-13 12:27:59 +0530

    Devops code committed.

---
+++ src/main/java/com/example/demo/DevopsSpringAppApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsSpringAppApplication.class, args);
+	}
+}
---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping(value="/",method=RequestMethod.GET)
+	public String Welcome() {
+		return "Welcome to Devops war file generation..!";
+	}
+}
